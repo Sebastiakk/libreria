6c304fb94b4f3008179fb3abde63a8882926f06d
Sebastian <sebastianramirezherrera65@gmail.com>
2019-05-25 11:02:37 -0500

    Inicio en libros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LibrosComponent } from './libros/libros.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'libros', component: LibrosComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BarComponent } from './bar/bar.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { LibrosComponent } from './libros/libros.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarComponent
+    BarComponent,
+    LibrosComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +29,11 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatCardModule,
+    MatTabsModule,
+    MatInputModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
